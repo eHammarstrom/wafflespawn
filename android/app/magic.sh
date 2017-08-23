df39973387c2dad5927747ea7d6dc60c57a173e8
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-08-23 16:48:13 +0200

    removed & re-generated certificates

--- android/app/magic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-KEYSTORE=waffle.jks
-ALIAS=waffleStore
-
-keytool -genkey -v -keystore $KEYSTORE -keyalg RSA -keysize 2048 -validity 10000 -alias $ALIAS
-
-keytool -exportcert -list -v -alias $ALIAS -keystore $KEYSTORE
